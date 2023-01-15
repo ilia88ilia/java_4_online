12fccc49903533f5e8857560933abd59ca2bf0d7
ilia88ilia <ilia.churkin88@gmail.com>
2023-01-15 20:25:59 +0200

    add hw_5_oop_and_tests

--- hw_5_oop_and_tests/run.sh
+++ hw_5_oop_and_tests/run.sh
@@ -1,7 +1,5 @@
 . ./build.sh
 
-java -jar C:/Users/PC/IdeaProjects/java_4_online/hw_5_oop_and_tests/target/hw_5_oop_and_tests.jar
-
-#java -jar /target/hw_5_oop_and_tests.jar
+java -jar /target/hw_5_oop_and_tests.jar
 
 

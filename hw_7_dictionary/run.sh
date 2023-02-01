09cc761254be387f004fcd7dc25093a68b1fe86f
ilia88ilia <91546729+ilia88ilia@users.noreply.github.com>
2023-02-01 03:55:31 +0200

    add hw_7_dictionary

--- hw_7_dictionary/run.sh
+++ hw_7_dictionary/run.sh
@@ -1,7 +1,5 @@
 . ./build.sh
 
-java -jar C:/Users/PC/IdeaProjects/java_4_online/hw_7_dictionary/target/hw_7_dictionary.jar
-
-#java -jar /target/hw_7_dictionary.jar
+java -jar /target/hw_7_dictionary.jar
 
 

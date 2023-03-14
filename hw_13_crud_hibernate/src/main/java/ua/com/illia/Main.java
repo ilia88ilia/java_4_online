7858cebbf6fed6657ca1dcf5ebda8917b62cbe10
ilia88ilia <91546729+ilia88ilia@users.noreply.github.com>
2023-03-14 16:09:31 +0200

    add hw_13_crud_hibernate

---
+++ hw_13_crud_hibernate/src/main/java/ua/com/illia/Main.java
@@ -0,0 +1,11 @@
+package ua.com.illia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//    Створити CRUD application на базі вашого першого модуля
+//    але використовуючи вже базову архітектуру (controller, service, dao, entity)
+//    В якості джерела збереження даних - використовуємо базу даних MySQL та Hibernate, як ORM провідер
